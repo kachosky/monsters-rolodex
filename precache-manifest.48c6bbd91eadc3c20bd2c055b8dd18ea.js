40198f9fc975b24b85380d85b4da9265243c9fe4
kachosky <vnoupelah@gmail.com>
2019-11-06 12:46:14 +0100

    Updates

---
+++ precache-manifest.48c6bbd91eadc3c20bd2c055b8dd18ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df6101b68f12d129c6339007474526c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c0b5c7b76794e9f0175",
+    "url": "/monsters-rolodex/static/css/main.a9b8bfaf.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "6c0b5c7b76794e9f0175",
+    "url": "/monsters-rolodex/static/js/main.6f9bf5c4.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
